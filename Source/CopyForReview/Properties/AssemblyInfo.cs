95e5cce1dd67f7ca100ac43b2f76973da6b280aa
Marcel <marcel+github@codeministry.ch>
2015-03-18 21:32:48 +0100

    Added Common Assembly

--- Source/CopyForReview/Properties/AssemblyInfo.cs
+++ Source/CopyForReview/Properties/AssemblyInfo.cs
@@ -8,29 +8,4 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CopyForReview")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("codeministry.ch")]
 [assembly: AssemblyProduct("CopyForReview")]
-[assembly: AssemblyCopyright("GPLv3")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]   
-[assembly: ComVisible(false)]     
-[assembly: CLSCompliant(false)]
-[assembly: NeutralResourcesLanguage("en-US")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
-
-[assembly: InternalsVisibleTo("CopyForReview_IntegrationTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010077bdf8ccbd3fcb3b738099839b753b7909a224a603d9308ad6942062610190d2d0a975b9cfa76dc9a47a48bee429f7e888f8f12e16a177fb443ff588101ac0e20299ffd7b116dee962edc0015553cf13cd3fdeb311066e0a4fa4eb11eda7a64e4b870bab272e2d3e1a69b5ead432dd1c3b28ab1f9a7b8bd00e62cc3564888da9")]
-[assembly: InternalsVisibleTo("CopyForReview_UnitTests, PublicKey=002400000480000094000000060200000024000052534131000400000100010077bdf8ccbd3fcb3b738099839b753b7909a224a603d9308ad6942062610190d2d0a975b9cfa76dc9a47a48bee429f7e888f8f12e16a177fb443ff588101ac0e20299ffd7b116dee962edc0015553cf13cd3fdeb311066e0a4fa4eb11eda7a64e4b870bab272e2d3e1a69b5ead432dd1c3b28ab1f9a7b8bd00e62cc3564888da9")]
