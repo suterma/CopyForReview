deaf51e818520eb5b79a46def9928bfed5ce9796
Marcel <marcel+github@codeministry.ch>
2015-03-20 13:34:01 +0100

    changed to .net 4.0, adapted vsxi (Also tried to support vs 2012)

--- Source/Codeministry.CopyForReview/Resources.Designer.cs
+++ Source/Codeministry.CopyForReview/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CopyForReview {
+namespace Codeministry.CopyForReview {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace CopyForReview {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CopyForReview.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Codeministry.CopyForReview.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
